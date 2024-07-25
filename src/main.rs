2f938e52d8d405d07a16cf4bbf5053c43084b66f
007tickooayush <ayush.tickoo292000@gmail.com>
2024-07-25 20:12:26 +0530

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("{}",calculate_weight_on_mars(74.0));
+}
+
+
+fn calculate_weight_on_mars(weight: f32) -> f32 {
+    (weight / 9.81) * 3.711
 }
