87035595d26f8ed1485385a50d9808cf440e3d21
007tickooayush <ayush.tickoo292000@gmail.com>
2024-07-29 16:51:36 +0530

    creating a TCP Server(pretend)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 fn main() {
-    println!("Please enter the weight in KGs:");
-    let mut weight = String::new();
-    std::io::stdin().read_line(&mut weight).unwrap();
-    println!();
+    let server = Server::new(String::from("127.0.0.1:8080"));
+    server.start();
 
-    let weight: f32 = weight.trim().parse().expect("A valid number not provided");
-    let weight = calculate_weight_on_mars(weight);
-
-    println!("The weight on mars in KGs: {}",weight);
+    // println!("Please enter the weight in KGs:");
+    // let mut weight = String::new();
+    // std::io::stdin().read_line(&mut weight).unwrap();
+    // println!();
+    //
+    // let weight: f32 = weight.trim().parse().expect("A valid number not provided");
+    // let weight = calculate_weight_on_mars(weight);
+    //
+    // println!("The weight on mars in KGs: {}",weight);
 }
 
+struct Server {
+    addr: String
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn start(&self) {
+        println!("Server started at: {}", self.addr);
+    }
+}
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
