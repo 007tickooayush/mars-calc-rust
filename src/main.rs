91235f00b5db6f311d983de1b6129c939fdddf1c
007tickooayush <ayush.tickoo292000@gmail.com>
2024-07-30 19:30:29 +0530

    code reformatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+mod request;
+mod method;
+mod utils;
+mod server;
+
 use std::io::{Error, Read};
 use std::net::TcpListener;
+use crate::server::Server;
 
 fn main() {
     let server = Server::new(String::from("127.0.0.1:7021"));
@@ -14,65 +20,4 @@ fn main() {
     // let weight = calculate_weight_on_mars(weight);
     //
     // println!("The weight on mars in KGs: {}",weight);
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    pub fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
-
-    pub fn start(&self) {
-        println!("Server started at: {}", self.addr);
-        let listener = TcpListener::bind(&self.addr).unwrap();
-
-        // using the Rust's infinite "loop" for listening to the incoming connection requests
-        loop {
-            match listener.accept() {
-                Ok((mut stream, addr)) => {
-                    println!("\nNew client: {}", addr);
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                        },
-                        Err(_) => {
-                            eprintln!("Failed to read from connection");
-                        }
-                    };
-                }
-                Err(e) => {
-                    eprintln!("Failed to establish a connection: {}", e);
-                }
-            }
-        }
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-
-}
-
-fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) * 3.711
 }
---
+++ src/server.rs
@@ -0,0 +1,40 @@
+use std::net::TcpListener;
+
+pub struct Server {
+    addr: String,
+}
+
+impl Server {
+    pub fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    pub fn start(&self) {
+        println!("Server started at: {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        // using the Rust's infinite "loop" for listening to the incoming connection requests
+        loop {
+            match listener.accept() {
+                Ok((mut stream, addr)) => {
+                    println!("\nNew client: {}", addr);
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(_) => {
+                            eprintln!("Failed to read from connection");
+                        }
+                    };
+                }
+                Err(e) => {
+                    eprintln!("Failed to establish a connection: {}", e);
+                }
+            }
+        }
+    }
+}
+
