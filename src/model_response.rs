ee00c293bc8074eeb52ee19e8f7df6e3d1a76a4b
007tickooayush <ayush.tickoo292000@gmail.com>
2024-08-02 22:12:34 +0530

    adding the Clone and Copy trait for StatusCode to return a u16

--- src/model_response.rs
+++ src/model_response.rs
@@ -1,6 +1,9 @@
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
 pub struct Response {
-    status_code : StatusCode,
-    body: Option<String>
+    status_code: StatusCode,
+    body: Option<String>,
 }
 
 impl Response {
@@ -9,8 +12,25 @@ impl Response {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
 pub enum StatusCode {
-    OK = 200,
-    BAD_REQUEST = 400,
-    NOT_FOUND = 404,
+    Ok = 200,
+    BadRequest = 400,
+    NotFound = 404,
+}
+
+impl StatusCode {
+    pub fn reason(&self) -> &str {
+        match self {
+            Self::Ok => "OK",
+            Self::BadRequest => "VAD REQUEST",
+            Self::NotFound => "NOT FOUND",
+        }
+    }
+}
+
+impl Display for StatusCode {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", *self as u16)
+    }
 }
